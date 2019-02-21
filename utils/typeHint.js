9e4fede48d7d25edfa8b222edf254607cd5bba40
flyc <yayayahahahaooii@gmail.com>
2019-02-21 16:30:48 +0800

    basic type detect without default stuff and array stuff works

--- utils/typeHint.js
+++ utils/typeHint.js
@@ -21,4 +21,31 @@ function typeHint(value, acceptTypes) {
         console.log('請至少輸入兩個參數: 第一個為欲被檢查的值，第二個為可接受的型態');
         return;
     }
+
+    var acceptTypesArray = [];
+
+    if (typeof acceptTypes === 'string') {
+        switch (acceptTypes) {
+            case 'string':
+            case 'number':
+            case 'boolean':
+            case 'function':
+            case 'undefined':
+                if (typeof value !== acceptTypes) {
+                    console.warn('型別錯誤: 想要的型別為 ' + acceptTypes + ' , 傳入的值的型別為' + typeof value);
+                    console.warn('沒有設定預設值，將回傳false');
+                    return false;
+                }
+                break;
+            case 'object':
+                break;
+            case 'array':
+                break;
+            default:
+                break;
+        }
+    }
 }
+
+var result = typeHint(undefined);
+console.log(result);
